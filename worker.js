f6a09c59b2dc3b5944f88cb2616b853c3c07344e
Amirwopi <56787008+Amirwopi@users.noreply.github.com>
2025-03-13 01:10:58 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,12 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
-const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
-const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_SECRET = "SIA_SECRET"; // Insert a powerful secret text and keep it safe.
-const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
+const BOT_TOKEN = "555310717:AAFLnE-Eq1l7CX5BsqeDhRskTZ0c0sh-RKI";
+const BOT_WEBHOOK = "/endpoint";
+const BOT_SECRET = "atased123";
+const BOT_OWNER = 304611718;
+const BOT_CHANNEL = -1002313727871;
+const SIA_SECRET = "test";
+const PUBLIC_BOT = false;
 
 // ---------- Do Not Modify ---------- // 
 
